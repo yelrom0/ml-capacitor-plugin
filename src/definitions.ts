786c9158acc1fc8bb8be82e5d9043e6b6e00935b
yelrom0 <yelrom0@gmail.com>
2024-02-04 06:49:49 +1100

    feat(Definitions): add extra definitions for image callback

--- src/definitions.ts
+++ src/definitions.ts
@@ -1,10 +1,26 @@
-export interface CapTorchPlugin {
-  echo(options: { value: string }): Promise<{ value: string }>;
-  loadImage(): Promise<{image: ImageData}>;
+export interface ImageAIData {
+  className: string;
+  confidence: number;
 }
 
 export interface ImageData {
   name: string;
   data: string;
   mimeType: string;
+}
+
+export type ImageResponseType = 'image' | 'ai';
+
+export interface ImageResponse {
+  id: string;
+  type: ImageResponseType;
+  data: ImageData | ImageAIData;
+}
+
+export type CallbackID = string;
+
+export type ImageResponseCallback = (response: ImageResponse) => void;
+export interface CapTorchPlugin {
+  echo(options: { value: string }): Promise<{ value: string }>;
+  loadImage(callback: ImageResponseCallback): Promise<CallbackID>;
 }
