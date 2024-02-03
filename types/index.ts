5d6bbbbfbfef43d1fa894e30ef1fd3ec4b957bd8
yelrom0 <yelrom0@gmail.com>
2024-02-04 06:50:23 +1100

    feat(Types): Add nicer export for types

---
+++ types/index.ts
@@ -0,0 +1,4 @@
+import {ImageAIData, ImageData, ImageResponse, CallbackID, CapTorchPlugin} from '../src/definitions'
+import type {ImageResponseType, ImageResponseCallback} from '../src/definitions'
+
+export { ImageAIData, ImageData, ImageResponseType, ImageResponse, CallbackID, ImageResponseCallback, CapTorchPlugin}
